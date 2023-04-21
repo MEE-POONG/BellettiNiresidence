9359ce9e5e8f18103f4e611f39193dd9737f7828
devilzeros <devilzeros@outlook.co.th>
2023-04-21 16:15:46 +0700

    est

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Navbar, Container, Nav, Button, NavDropdown, Image } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, NavDropdown, Image, Offcanvas } from 'react-bootstrap';
 
 export default function NavigationBar(): JSX.Element {
   const { asPath } = useRouter();
   const [navbarTop, setNavbarTop] = useState<boolean>(false);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
@@ -21,40 +25,50 @@ export default function NavigationBar(): JSX.Element {
     }
   };
   return (
-    <Navbar expand="lg" bg="white" className="py-lg-0 px-4 px-lg-5">
+    <Navbar expand="lg" bg="white" className="py-0">
       <Container>
-        <Navbar.Brand href="/" className="ps-0 ms-0 py-2 px-4 mx-3">
+        <Navbar.Brand href="/" className="ps-0 ms-0 py-2 px-4 mx-3" >
           <Image src="images/logo.png" alt="logo" className='logo' />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-        <Navbar.Collapse id="navbarCollapse" className="justify-content-between py-4 py-lg-0">
-          <Nav className="ms-auto py-0 text-center f-f-domine">
-            <Nav.Link href="/" className="active">
-              THE BUILDING
-            </Nav.Link>
-            <Nav.Link href="/residences">
-              RESIDENCES
-            </Nav.Link>
-            <Nav.Link href="/roomtype">
-              ROOMTYPE
-            </Nav.Link>
-            <Nav.Link href="/floorplan">
-              FLOOR PLANS
-            </Nav.Link>
-            <Nav.Link href="/amenities">
-              AMENITIES
-            </Nav.Link>
-            <Nav.Link href="/engage">
+        <Navbar.Toggle onClick={handleShow} aria-controls={`offcanvasNavbar-expand-lg`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-lg`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+          placement="top"
+          className="overflowX-auto"
+        >
+          <Offcanvas.Header closeButton>
+            <Image src="images/logo.png" alt="logo" className='logo-nav' />
+          </Offcanvas.Header>
+          <Offcanvas.Body bsPrefix='navbar-nav'>
+            <Nav className="f-f-domine">
+              <Link href="/" className="nav-link active">
+                THE BUILDING
+              </Link>
+              <Link href="/residences" className="nav-link">
+                RESIDENCES
+              </Link>
+              <Link href="/roomtype" className="nav-link">
+                ROOMTYPE
+              </Link>
+              <Link href="/floorplan" className="nav-link">
+                FLOOR PLANS
+              </Link>
+              <Link href="/amenities" className="nav-link">
+                AMENITIES
+              </Link>
+              {/* <Link href="/engage" className="nav-link">
               ENGAGE
-            </Nav.Link>
-            <Nav.Link href="/inquire">
-              INQUIRE
-            </Nav.Link>
-            <Nav.Link href="/gallery">
-              GALLERY
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+            </Link> */}
+              <Link href="/inquire" className="nav-link">
+                INQUIRE
+              </Link>
+              <Link href="/gallery" className="nav-link">
+                GALLERY
+              </Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
 
