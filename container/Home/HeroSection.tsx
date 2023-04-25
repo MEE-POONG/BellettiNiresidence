831843af71d1f3384296e23e98288f521a341466
devilzeros <devilzeros@outlook.co.th>
2023-04-26 01:40:17 +0700

    est

--- container/Home/HeroSection.tsx
+++ container/Home/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import AOS from 'aos';
 import { Container, Row, Col, Button, Image, Modal } from 'react-bootstrap';
+import { Parallax } from 'react-parallax';
 
 const HeroSection: React.FC = () => {
   useEffect(() => {
@@ -13,35 +14,43 @@ const HeroSection: React.FC = () => {
   const handleShowModal = () => setShowModal(true);
   const handleCloseModal = () => setShowModal(false);
   return (
-    <Container fluid className="hero-header bg-light py-5 mb-5">
-      <Container className="py-5">
-        <Row className="g-5 align-items-center">
-          <Col lg={6}>
-            <p className="text-primary text-uppercase mb-2 animated slideInDown">Bellettini Residence</p>
-            <h1 className="display-4 mb-3 animated slideInDown">Bellettini Residence</h1>
-            <p className="animated slideInDown">
-              An unprecedented approach, bellettiniresidence glass façade has been cut in a playful rhythm around the building to create a captivating visual point in the skyline with a multitude of open spaces for uninterrupted views of the natural surroundings.
-            </p>
-            <div className="d-flex align-items-center pt-4 animated slideInDown">
-              <Button variant="outline-light" className="btn-play" onClick={handleShowModal}>
-                <span></span>
-              </Button>
-              <h5 className="ms-4 mb-0 d-none d-sm-block">Play Video</h5>
-            </div>
-          </Col>
-          <Col lg={6} className="animated fadeIn">
-            <Row className="g-3">
-              <Col xs={6} className="text-end">
-                <Image className="img-fluid bg-white p-3 w-100 mb-3" src="images/hero-1.jpg" alt="" />
-                <img className="img-fluid bg-white p-3 w-100" src="images/hero-2.jpg" alt="" />
-              </Col>
-              <Col xs={6}>
-                <img className="img-fluid bg-white p-3 w-100 mb-3" src="images/hero-4.jpg" alt="" />
-                <img className="img-fluid bg-white p-3 w-100" src="images/hero-3.jpg" alt="" />
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+
+    <Parallax
+      bgImage="./images/hero-1.jpg"
+      strength={-500}
+      className='hero-header'
+    >
+      <div className='bg-filter' />
+      <Container fluid className='py-5'>
+        <Container className="py-5 score">
+          <Row className="g-5 align-items-center">
+            <Col lg={6}>
+              <p className="text-primary text-uppercase mb-2 animated slideInDown">Bellettini Residence</p>
+              <h1 className="display-4 mb-3 animated slideInDown text-white">Bellettini Residence</h1>
+              <p className="animated slideInDown text-white">
+                An unprecedented approach, bellettiniresidence glass façade has been cut in a playful rhythm around the building to create a captivating visual point in the skyline with a multitude of open spaces for uninterrupted views of the natural surroundings.
+              </p>
+              <div className="d-flex align-items-center pt-4 animated slideInDown">
+                <Button variant="outline-light" className="btn-play" onClick={handleShowModal}>
+                  <span></span>
+                </Button>
+                <h5 className="ms-4 mb-0 d-none d-sm-block text-white">Play Video</h5>
+              </div>
+            </Col>
+            <Col lg={6} className="animated fadeIn">
+              <Row className="g-3">
+                <Col xs={6} className="text-end">
+                  <Image className="img-fluid bg-white p-3 w-100 mb-3" src="images/hero-1.jpg" alt="" />
+                  <img className="img-fluid bg-white p-3 w-100" src="images/hero-2.jpg" alt="" />
+                </Col>
+                <Col xs={6}>
+                  <img className="img-fluid bg-white p-3 w-100 mb-3" src="images/hero-4.jpg" alt="" />
+                  <img className="img-fluid bg-white p-3 w-100" src="images/hero-3.jpg" alt="" />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
       </Container>
       <Modal show={showModal} onHide={handleCloseModal} size="lg" centered>
         <Modal.Body className="p-0">
@@ -62,7 +71,9 @@ const HeroSection: React.FC = () => {
           </Button>
         </Modal.Footer>
       </Modal>
-    </Container>
+    </Parallax>
+
+
   );
 };
 
